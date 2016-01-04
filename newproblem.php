10f2cbfb0dabc77686c75cab1410bc27aa0e4c33
Dodd <Dodd@Dodd2014.com>
2016-01-04 16:25:49 +0800

    js ok!

--- newproblem.php
+++ newproblem.php
@@ -121,12 +121,32 @@ if ($_SESSION['img'] == '') {
       </div>
       <!-- 999999999999999999999999999999999999999999999999999999999 -->
       <div class="am-u-sm-12 am-u-md-8 am-u-md-pull-4">
-        <form method="post" action="Notification.php" >
-          <textarea name="textarea" cols="40" rows="10" placeholder="Type the notification."></textarea>
-          <br>
-          <br>
-          <input type="submit" value="submit">
-        </form>
+        <div class="am-u-sm-centered">
+          <form class="am-form" method="post" action="newproblem.php" id='form1'>
+            <div class="am-form-group">
+                  <label for="doc-ta-1">Description:</label>
+                  <textarea class="" rows="5" id="doc-ta-1" name="des" placeholder="Description"></textarea>
+            </div>
+            <div class="am-btn-group doc-js-btn-1" data-am-button>
+              <label class="am-btn am-btn-primary">
+                <input type="radio" name="options" value="type1" id="option1"> Radio Problem
+              </label>
+              <label class="am-btn am-btn-primary">
+                <input type="radio" name="options" value="type2" id="option2"> Checkbox Problem
+              </label>
+              <label class="am-btn am-btn-primary am-active">
+                <input type="radio" name="options" value="type3" id="option3"> Write
+              </label>
+<!--               <label class="am-btn am-btn-primary am-disabled">
+                <input type="radio" name="options" value="选项 4" id="option4"> 选项 4
+              </label> -->
+            </div>
+            <div id='adddiv'>
+              
+            </div>
+            <p><button type="submit" class="am-btn am-btn-default">Submit</button></p>
+          </form>
+        </div>
       </div>
     </div>
   </div>
@@ -153,5 +173,41 @@ if ($_SESSION['img'] == '') {
 <script src="assets/js/amazeui.min.js"></script>
 
 <script src="assets/js/app.js"></script>
+<script>
+  var checked=3;
+  var flag=0;
+  var num=1;
+  function addbtn(){
+    var add="<div id='addansdiv'><a id='addansbtn' href='##' class='am-icon-md am-secondary am-icon-plus-square'></a><label>   Click plus to add answer...</label></div>";
+    $("#adddiv").after(add);
+    $("#addansbtn").click(function(){
+      add="<input class='am-form-field am-input-sm' style='margin:5px;' type='text' placeholder='Enter the answer"+num+". ' name='ans"+num+"'>";
+      num++;
+      $("#adddiv").append(add);
+      flag=1;
+    });
+  }
+  $(document).ready(function() {
+        $("input[name='options']").change(function() {
+          var $selectedvalue = $("input[name='options']:checked").val();
+           //alert($selectedvalue);
+           if(flag){
+            $("#adddiv").html("");
+            $("#addansdiv").remove();
+            num=1;
+          }
+          if ($selectedvalue == 'type1') {
+            //alert('1');
+              addbtn();
+          }
+          if ($selectedvalue == 'type2') {
+            //alert('2');
+              addbtn();
+          }
+          if ($selectedvalue == 'type3') {
+          }
+        });
+  });
+</script>
 </body>
 </html>
